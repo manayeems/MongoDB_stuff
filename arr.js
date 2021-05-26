e55649e3192892cf68be0039ae1e81b13ffcc73a
Mohammed Nayeem <84686694+manayeems@users.noreply.github.com>
2021-05-25 20:37:20 -0400

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,25 @@
+// load('/usr/local/mongodb/arr.js')
+
+var arr = [
+    {
+        list:'UK',
+        cities:['London','Glasgow','Cardiff','Newcastle','Durham','Manchester','Coventry']
+    },
+    {
+        list:'US',
+        cities:['New York','Washington','LA','Las Vegas','Miami','Seattle','Dallas']
+    },
+    {
+        list:'Australia',
+        cities:['Sydney','Melbourne','Perth','Outback']
+    },
+    {
+        list:'England',
+        cities:['London','Newcastle']
+    },
+    {
+        list:'Northumberland',
+        cities:['Newcastle','Durham']
+    }
+]
+
---
+++ nested.js
@@ -0,0 +1,27 @@
+// load('/usr/local/mongodb/nested.js')
+
+var nested = [
+    {
+        brand:'Toyota',
+        models:[
+            { model:'Land Cruiser',engine:'Diesel',gearbox:'Auto' },
+            { model:'Supra',engine:'Petrol',gearbox:'Manual' },
+            { model:'Carolla',engine:'Petrol',gearbox:'CVT' }
+        ]
+    },
+    {
+        brand:'Honda',
+        models:[
+            { model:'CR-V',engine:'Hybrid',gearbox:'CVT' },
+            { model:'Civic',engine:'Diesel',gearbox:'Manual' },
+            { model:'Jazz',engine:'Petrol',gearbox:'manual' }
+        ]
+    },
+    {
+        brand:'Lexus',
+        models:[
+            { model:'LF-A',engine:'Petrol',gearbox:'Auto' },
+            { model:'LX570',engine:'Petrol',gearbox:'Auto' }
+        ]
+    }
+]
